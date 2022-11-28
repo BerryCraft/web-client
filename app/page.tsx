a967e1af615bb1a6167716bb0f2f923e7e9b49b0
cofeek-codes <kormyshev11@mail.ru>
2022-11-28 12:22:28 +0500

    set background

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,13 @@ import { images } from '@/helpers/imageLoader'
 import styles from './index.module.scss'
 
 const HomePage = () => {
-	return <News />
+	return (
+		<div className={styles.background}>
+			<div className={styles.wrapper}>
+				<News />
+			</div>
+		</div>
+	)
 }
 
 export default HomePage
--- components/index/news/News.tsx
+++ components/index/news/News.tsx
@@ -2,7 +2,7 @@ import { images } from '@/helpers/imageLoader'
 import styles from './news.module.scss'
 const News = () => {
 	return (
-		<div className={styles.wrapper} style={{}}>
+		<div className={styles.wrapper}>
 			<div className={styles.title}>Новости</div>
 			<div className={styles.field}></div>
 		</div>
