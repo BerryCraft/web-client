be8dc6989886d99f0bbffb51c56e0b6501b47a7f
Faelorg <efremovdanil1981@gmail.com>
2023-04-01 11:39:39 +0500

    prepared for migration

---
+++ zu-store/user.slice.ts
@@ -0,0 +1,20 @@
+import { User } from '@/types/entities/user.entity'
+import { create } from 'zustand'
+
+interface UserStore {
+  currentUser: User | null
+  login: (credentials: User) => void
+  logout: () => void
+}
+
+const useUserStore = create<UserStore>(set => ({
+  currentUser: null,
+  login: (user: User) =>
+    set(state => ({
+      currentUser: user,
+    })),
+  logout: () =>
+    set(state => ({
+      currentUser: null,
+    })),
+}))
