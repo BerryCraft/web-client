8557236aa3b943a4515e2a6fbab71df3fcaa7c25
cofeek-codes <kormyshev11@mail.ru>
2023-04-23 02:24:22 +0500

    need to improve error handling

--- components/auth/Login.tsx
+++ components/auth/Login.tsx
@@ -1,11 +1,18 @@
 import { useActions } from '@/hooks/useActions'
+import { useAuth } from '@/hooks/useAuth'
+import styles from '@/styles/components/auth/login.module.scss'
 import { LoginDTO } from '@/types/dto/login.dto'
 import { SubmitHandler, useForm } from 'react-hook-form'
-
 const Login = () => {
+	const { isLoading } = useAuth()
 	const { login } = useActions()
 
-	const { register, handleSubmit, formState, reset } = useForm<LoginDTO>({
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+		reset,
+	} = useForm<LoginDTO>({
 		mode: 'onChange',
 	})
 	const handleLogin: SubmitHandler<LoginDTO> = data => {
@@ -13,7 +20,30 @@ const Login = () => {
 		reset()
 	}
 
-	return <div>Login</div>
+	return (
+		<div className={styles.wrapper}>
+			<form onSubmit={handleSubmit(handleLogin)}>
+				<div className='field__title'>Логин</div>
+				<input
+					type='text'
+					placeholder='Логин'
+					{...register('login', { required: 'login is required' })}
+				/>
+				{errors.login && <p>{errors.login.message}</p>}
+				<div className='field__title'>Пароль</div>
+				<input
+					type='password'
+					placeholder='Пароль'
+					{...register('password', { required: 'password is required' })}
+				/>
+				{errors.login && <p>{errors.login.message}</p>}
+
+				<button type='submit' className='login__button'>
+					Войти
+				</button>
+			</form>
+		</div>
+	)
 }
 
 export default Login
